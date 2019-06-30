f6975147f5eb52f617c29467418a5bf653fac3d3
getlike <arsentiyscherbakov@gmail.com>
2019-06-30 23:07:11 +0300

    Default Changelist

--- 15Home/chat/api/users.php
+++ 15Home/chat/api/users.php
@@ -7,35 +7,61 @@ include '../configs/db.php';
 //последнее сообщение для айди
 //SELECT * FROM messages WHERE user_id = 6 ORDER BY created DESC LIMIT 1;.
 //getLastMessage(1,$conn);
-function getLastMessage($user_id,$conn){
-    $sql="SELECT * FROM messages WHERE user_id = ".$user_id." ORDER BY created DESC LIMIT 1;";
-    $result=$conn->query($sql);
-    if ($result->num_rows>0){
-         $row=$result->fetch_assoc();
+function getLastMessage($user_id, $conn)
+{
+    //
+    $sql = "SELECT * FROM messages WHERE user_id = " . $user_id . " ORDER BY created DESC LIMIT 1;";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
         echo json_encode(["lastMessage" => $row]);
     }
 
 }
-//getAllMessage(6,$conn);
-function getAllMessage($user_id,$conn){
-    $sql="SELECT messages.message,messages.created, messages.user_id,users.username,users.photo
+
+getAllUsers($conn);//без этого не работает О_О люблю php (срказьм)
+function getAllUsers($conn)
+{
+    $sql = "SELECT * FROM users";
+    $result = $conn->query($sql);
+    $users = [];
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+
+            $users[] = [
+                "id" => $row['id'],
+                "username" => $row['username'],
+                "photo" => $row['photo'],
+                "email" => $row['email'],
+                "role" => $row['role']
+            ];
+        }
+
+    }
+    echo json_encode(["users" => $users]);
+}
+
+function getAllMessage($user_id, $conn)
+{
+    $sql = "SELECT messages.message,messages.created, messages.user_id,users.username,users.photo
           FROM messages 
           JOIN users 
           WHERE users.id=user_id 
-          AND user_id=".$user_id." ;";
+          AND user_id=" . $user_id . " ;";
     $result = $conn->query($sql);
     $messages = [];
 
-    if($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()) {
-           /*
-            * Array
-(
-    [message] => asCsefgzsefzfzsefsezfsef
-    [created] => 2019-06-22 14:14:57
-    [user_id] => 6
-    [username] => test
-)*/
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            /*
+             * Array
+ (
+     [message] => asCsefgzsefzfzsefsezfsef
+     [created] => 2019-06-22 14:14:57
+     [user_id] => 6
+     [username] => test
+ )*/
             $messages[] = [
                 "message" => $row['message'],
                 "created" => $row['created'],
@@ -45,84 +71,123 @@ function getAllMessage($user_id,$conn){
         }
     }
 
-    echo json_encode( $messages);
+    echo json_encode($messages);
+}
+
+//writeMessage(3,'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',$conn);
+
+function writeMessage($user_id,$message,$conn){
+    $sql = "INSERT INTO messages (user_id,message) VALUES ('".$user_id."', '".$message."');";
+    $conn->query($sql);
+//    echo $sql;
 }
 // Получить всех друзей выбранного пользователя
-function getFriends($user_id, $conn) {
+//неработат. Може вы что то подскажете.
+if (isset($_POST['page']) && $_POST['page'] == 'writeMessage') {
 
-	$sql = "SELECT * FROM friends WHERE user_1=$user_id";
-	$result = $conn->query($sql);
-	$friends = [];	
-	if($result->num_rows > 0) {
-		while($row = $result->fetch_assoc()) {
 
-			$sql = "SELECT * FROM users WHERE id=" . $row['user_2'];
-			$resultFriend = $conn->query($sql);
-			if($resultFriend->num_rows > 0) {
-				$friend = $resultFriend->fetch_assoc();
-				$friends[] = [
-					"id" => $friend['id'],
-					"username" => $friend['username'],
-					"photo" => $friend['photo'],
-					"email" => $friend['email']
-				];
-			}
-		}
-	}
+    writeMessage($_POST['user_id'],$_POST['message'],$conn);
 
-	echo json_encode(["friends" => $friends]);
+
+
+
 }
+
+function getFriends($user_id, $conn)
+{
+
+    $sql = "SELECT * FROM friends WHERE user_1=$user_id";
+    $result = $conn->query($sql);
+    $friends = [];
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+
+            $sql = "SELECT * FROM users WHERE id=" . $row['user_2'];
+            $resultFriend = $conn->query($sql);
+            if ($resultFriend->num_rows > 0) {
+                $friend = $resultFriend->fetch_assoc();
+                $friends[] = [
+                    "id" => $friend['id'],
+                    "username" => $friend['username'],
+                    "photo" => $friend['photo'],
+                    "email" => $friend['email']
+                ];
+            }
+        }
+    }
+
+    echo json_encode(["friends" => $friends]);
+}
+
 /*
 * @todo как оптимизировать код, убрать дублирование пользователей
 */
-function searchUser($text, $conn) {
-	$sql = "SELECT * FROM `users` WHERE `username` LIKE '%" . $text . "%'";
-	$result = $conn->query($sql);
-	$users = [];	
+function searchUser($text, $conn)
+{
+    $sql = "SELECT * FROM `users` WHERE `username` LIKE '%" . $text . "%'";
+    $result = $conn->query($sql);
+    $users = [];
 
-	if($result->num_rows > 0) {
-		while($row = $result->fetch_assoc()) {
-			$users[] = [
-				"id" => $row['id'],
-				"username" => $row['username'],
-				"photo" => $row['photo'],
-				"email" => $row['email']
-			];
-		}
-	}
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $users[] = [
+                "id" => $row['id'],
+                "username" => $row['username'],
+                "photo" => $row['photo'],
+                "email" => $row['email']
+            ];
+        }
+    }
 
-	echo json_encode(["users" => $users]);
+    echo json_encode(["users" => $users]);
 }
 
-function addFriend($current_user, $user_id, $conn) {
-	$sql = "INSERT INTO `friends` (`user_1`, `user_2`, `status`) VALUES ('$current_user', '$user_id', 'true')";
-	
-	if ($conn->query($sql) === TRUE) {
-	    echo json_encode(["status" => "ok"]);
-	} else {
-	    echo json_encode(["status" => "error"]);
-	}
+function addFriend($current_user, $user_id, $conn)
+{
+    $sql = "INSERT INTO `friends` (`user_1`, `user_2`, `status`) VALUES ('$current_user', '$user_id', 'true')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo json_encode(["status" => "ok"]);
+    } else {
+        echo json_encode(["status" => "error"]);
+    }
 }
 
 /*
 * @todo сделать через switch
 */
-if(isset($_GET['page']) && $_GET['page'] == 'friends') {
-	getFriends($_GET['current_user'], $conn); // !!! $conn !!! -> ** как сделать $conn глобальной
+if (isset($_GET['page']) && $_GET['page'] == 'friends') {
+    getFriends($_GET['current_user'], $conn); // !!! $conn !!! -> ** как сделать $conn глобальной
 }
 
-if(isset($_GET['page']) && $_GET['page'] == "search" && isset($_GET['search'])) {
-	searchUser($_GET['search'], $conn);
+if (isset($_GET['page']) && $_GET['page'] == "search" && isset($_GET['search'])) {
+    searchUser($_GET['search'], $conn);
 }
 
 
-if(isset($_POST['page']) && $_POST['page'] == 'add_friend') {
-	addFriend($_POST['current_user'], $_POST['user_id'], $conn);
+if (isset($_POST['page']) && $_POST['page'] == 'add_friend') {
+    addFriend($_POST['current_user'], $_POST['user_id'], $conn);
 }
-if(isset($_POST['page']) && $_POST['page'] == 'last_message') {
-    getLastMessage($_POST['user_id'],  $conn);
+if (isset($_POST['page']) && $_POST['page'] == 'last_message') {
+    getLastMessage($_POST['user_id'], $conn);
 }
 
-if(isset($_POST['page']) && $_POST['page'] == 'getAllMessage') {
-    getAllMessage($_POST['user_id'],  $conn);
+if (isset($_POST['page']) && $_POST['page'] == 'getAllMessage') {
+    getAllMessage($_POST['user_id'], $conn);
+}
+if (isset($_POST['page']) && $_POST['page'] == 'getAllUsers') {
+    getAllUsers($conn);
+}
+if (isset($_POST['page']) && $_POST['page'] == 'writeMessage') {
+    $fd = fopen("error.txt", 'w') or die("не удалось создать файл");
+    $str = $_POST['user_id']." ".$_POST['message'];
+    fwrite($fd, $str);
+    fclose($fd);
+
+
+    writeMessage($_POST['user_id'],$_POST['message'],$conn);
+
+
+
+
 }
--- 15Home/chatApp/www/js/index.js
+++ 15Home/chatApp/www/js/index.js
@@ -2,120 +2,125 @@ var app = {
     searchInput: null,
     searchBtn: null,
     // Application Constructor
-    initialize: function() {
+    initialize: function () {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
 
-   
-    onDeviceReady: function() {
+
+    onDeviceReady: function () {
 
         this.searchInput = document.querySelector('.search-input');
         this.searchBtn = document.querySelector('img.search');
-        this.backBtn=document.querySelector('.back');
+        this.backBtn = document.querySelector('.back');
+
+
 
         var that = this;
-       
+        var personalBtn = document.querySelector('.user-ic');
+
+            personalBtn.addEventListener('click',function () {
+                window.location.href = "/personal.html";
+            });
         // открытие поля поиска
-        this.searchBtn.addEventListener("click", function() {
+        this.searchBtn.addEventListener("click", function () {
             var pageTitle = document.querySelector(".page-title");
-                pageTitle.style.display = "none";
-                
-                that.searchInput.style.display = "block";
+            pageTitle.style.display = "none";
+
+            that.searchInput.style.display = "block";
         });
 
         //сбросить поиск
-        this.backBtn.addEventListener('click',function () {
-           console.dir(that.backBtn);
+        this.backBtn.addEventListener('click', function () {
+            document.location.href = '/allUsers.html';
         });
         // поиск контактов
-        this.searchInput.addEventListener("keyup", function(e) {
+        this.searchInput.addEventListener("keyup", function (e) {
             var contentBlock = document.querySelector(".content");
-                contentBlock.innerHTML = "";
+            contentBlock.innerHTML = "";
 
             var xml = new XMLHttpRequest();
-                xml.open("GET", 'http://15home/chat/api/users.php?page=search&search=' + e.target.value, false);
-                xml.send();
+            xml.open("GET", 'http://15home/chat/api/users.php?page=search&search=' + e.target.value, false);
+            xml.send();
 
             var users = JSON.parse(xml.response);
-            
-            for(var i = 0; i < users.users.length; i++) {
+
+            for (var i = 0; i < users.users.length; i++) {
                 that.generateContact(users.users[i]);
             }
         });
 
 
-
         this.getFriends();
-        
+
     },
 
-    getFriends: function() {
-        
-       var xml = new XMLHttpRequest();
-           xml.open("GET", 'http://15home/chat/api/users.php?current_user=5&page=friends', false);
-           xml.send();
+    getFriends: function () {
+
+        var xml = new XMLHttpRequest();
+        xml.open("GET", 'http://15home/chat/api/users.php?current_user=5&page=friends', false);
+        xml.send();
 
         var users = JSON.parse(xml.response);
-        
-        for(var i = 0; i < users.friends.length; i++) {
+
+        for (var i = 0; i < users.friends.length; i++) {
             this.generateContact(users.friends[i]);
         }
 
     },
 
-    generateContact: function(user) {
+    generateContact: function (user) {
         var contentBlock = document.querySelector(".content");
 
 
         var dialog = document.createElement('div');
-            dialog.className = "dialog";
-        dialog.addEventListener('click',function () {
-            window.location.href = "/allMessage.html?user_id="+user.id;
+        dialog.className = "dialog";
+        dialog.addEventListener('click', function () {
+            window.location.href = "/allMessage.html?user_id=" + user.id;
         })
         var avatar = document.createElement("img");
-            avatar.className = "dialog-avatar";
-            avatar.src = user.photo;
+        avatar.className = "dialog-avatar";
+        avatar.src = user.photo;
         var textBlock = document.createElement("div");
-            textBlock.className = "text-block";
-            textBlock.innerHTML = "<div class=\"title-block\">" + 
-                                        "<b class='dialog-title'>" + user.username + "</b>" + 
-                                        '<span class="no-read-msg-indicator">2</span>' +
-                                    "</div>" +
-                                    "<span class=\"dialog-last-msg\">"+lastMessage(user.id)+"</span>";
-           // lastMessage(1);
-           //lastMessage post // записываем последнее сообщение
-            function lastMessage(user_id) {
-                var xml=new XMLHttpRequest();
-                xml.open("POST","http://15home/chat/api/users.php",false);
-                xml.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-                xml.send("page=last_message&user_id=" + user.id);
-                try {//не у всех есть сообщения(((((
-                    var lastMessage = JSON.parse(xml.response);
-                    return lastMessage.lastMessage.message;
+        textBlock.className = "text-block";
+        textBlock.innerHTML = "<div class=\"title-block\">" +
+            "<b class='dialog-title'>" + user.username + "</b>" +
+            '<span class="no-read-msg-indicator">2</span>' +
+            "</div>" +
+            "<span class=\"dialog-last-msg\">" + lastMessage(user.id) + "</span>";
+        // lastMessage(1);
+        //lastMessage post // записываем последнее сообщение
+        function lastMessage(user_id) {
+            var xml = new XMLHttpRequest();
+            xml.open("POST", "http://15home/chat/api/users.php", false);
+            xml.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            xml.send("page=last_message&user_id=" + user.id);
+            try {//не у всех есть сообщения(((((
+                var lastMessage = JSON.parse(xml.response);
+                return lastMessage.lastMessage.message;
 
-                }
-                catch {
-                    return '';
-                }
-
+            } catch {
+                return '';
             }
+
+        }
+
         //lastMessage post===========================================================
         var addContactBtn = document.createElement("button");
-            addContactBtn.addEventListener('click', function() {
-                var xml = new XMLHttpRequest();
-                    xml.open("POST", "http://15home/chat/api/users.php", false);
-                    xml.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-                    xml.send("page=add_friend&current_user=17&user_id=" + user.id);
-                console.dir(xml);
-            });
-            textBlock.appendChild(addContactBtn);
+        addContactBtn.addEventListener('click', function () {
+            var xml = new XMLHttpRequest();
+            xml.open("POST", "http://15home/chat/api/users.php", false);
+            xml.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            xml.send("page=add_friend&current_user=17&user_id=" + user.id);
+            console.dir(xml);
+        });
+        textBlock.appendChild(addContactBtn);
         var dialogTime = document.createElement("span");
-            dialogTime.className = "dialog-time";
-            dialogTime.innerText = "20:02";
+        dialogTime.className = "dialog-time";
+        dialogTime.innerText = "20:02";
 
         //1. В мобильном приложении сделай отображение сообщений выбранного пользователя.
-        dialog.addEventListener('click',function () {
-           console.log('push me) '+user.id);
+        dialog.addEventListener('click', function () {
+            console.log('push me) ' + user.id);
         });
         //=====================================================================
 
@@ -126,6 +131,7 @@ var app = {
         contentBlock.appendChild(dialog);
     }
     // window.location.href = "http://www.w3schools.com";
+
 };
 
 app.initialize();
